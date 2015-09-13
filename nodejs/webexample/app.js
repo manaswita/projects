b8612733e76e54350d1872514eb76b7c42ca5331
unknown <manaswita.mishra@gmail.com>
2015-09-13 23:29:02 +0530

    nodejs

---
+++ nodejs/webexample/app.js
@@ -0,0 +1,47 @@
+var express= require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app =  express();
+//config app
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//use middleware
+
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'bower-components')));
+
+//define routes
+
+var todoItems=[
+		{id:1, desc:'foo'},
+		{id:2, desc:'bar'},
+		{id:3, desc:'baz'},
+	];
+
+app.get('/', function(req, resp) {
+	//load data from DB here
+resp.render('index',{
+	title: "Mansi",
+	items: todoItems
+});
+});
+
+app.post('/add', function(req, res){
+	var newItem= req.body.newItem;
+	todoItems.push({
+		id: todoItems.length + 1,
+		desc: newItem
+	});
+	res.redirect('/');
+});
+
+app.get('*', function(){
+res.send(405,'Method not allowed');
+});
+
+app.listen(1337, function() {
+console.log('working');
+});
